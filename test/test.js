be418a5dcf8d3f38fa80298bc71aaa70583b5b41
Josh Duff <me@JoshDuff.com>
2017-02-14 11:57:48 -0600

    A failing test for #1

--- test/test.js
+++ test/test.js
@@ -37,3 +37,42 @@ test('render something', t => {
 		t.end()
 	})
 })
+
+test('render something with code', t => {
+	const state = makeTestState()
+
+	state.retrieval.addPost('post', { title: 'TEMPLAAAATE', markdown: false }, '{{>current}}')
+	state.retrieval.addPost('file1.md', { title: 'Some title', date: new Date() }, `
+# This is a header
+
+<p>This is a legit paragraph</p>
+
+    <p>This is html inside of a code block</p>
+`)
+
+	const lazyRender = createLazyRenderer({
+		butler: state.butler,
+		data: {
+			externalValue: 'yarp'
+		},
+		indexHtml: '<body>{{{html}}}</body>'
+	})
+
+	lazyRender({
+		file: 'file1.md',
+		sessionData: {
+			sessionValue: 'blurp'
+		}
+	}).then(html => {
+		t.equal(html, `<body><h1 id="this-is-a-header">This is a header</h1>
+<p>This is a legit paragraph</p>
+<pre><code>&lt;p&gt;This is html inside of a code block&lt;/p&gt;
+</code></pre>
+</body>`)
+		t.end()
+	}).catch(e => {
+		t.fail('Promise resolved to an error')
+		console.error(e)
+		t.end()
+	})
+})
